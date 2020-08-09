42e4cf98a36b45e6c9176a4000c4a8dfba4984e8
Alexei Bykov <alexei4203@yandex.ru>
2020-08-09 15:20:48 +0300

    optimize line width/weight calculation

--- wrap.py
+++ wrap.py
@@ -2,17 +2,7 @@ import argparse
 import sys
 
 
-MAX_WEIGHT = 1000000000
-
-
-def calc_line_width(words, line_range):
-    if not line_range:
-        return 0
-
-    width = len(line_range) - 1 # necessary spaces
-    for i in line_range:
-        width += len(words[i])
-    return width
+MAX_WEIGHT = 1e100
 
 
 def calc_line_weight(width, column_width, is_last_line):
@@ -48,25 +38,40 @@ def format_line(words, line_range, width, column_width):
     return ''.join(tokens)
 
 
-class SuffixSolution:
-    def __init__(self, words, first_line_range, width, weight, next_line_suffix):
+class Line:
+    def __init__(self, words, start, end, width):
+        if start > end:
+            raise Exception(f"invalid Line range: {start}, {end}")
         self.words = words
-        self.first_line_width = width
-        self.first_line_range = first_line_range
-        self.suffix_weight = weight
-        self.next_line_suffix = next_line_suffix
+        self.start = start
+        self.end = end
+        self.width = width
 
-    def weight(self):
-        return self.suffix_weight
+    def calc_weight(self, column_width):
+        return calc_line_weight(self.width, column_width, self.end == len(self.words))
 
-    def first_line(self, column_width):
-        return format_line(self.words, self.first_line_range, self.first_line_width, column_width)
+    def expand_right(self):
+        if self.end < len(self.words):
+            self.width += len(self.words[self.end])
+            if self.end > self.start:
+                self.width += 1 # put a space in between
+            self.end += 1
 
-    def next(self):
-        return self.next_line_suffix
+    def copy(self):
+        return Line(self.words, self.start, self.end, self.width)
 
     def empty(self):
-        return len(self.first_line_range) == 0
+        return self.start >= self.end
+
+    def format(self, column_width):
+        return format_line(self.words, range(self.start, self.end), self.width, column_width)
+
+
+class SuffixSolution:
+    def __init__(self, first_line, weight, next_suffix):
+        self.first_line = first_line
+        self.weight = weight
+        self.next_suffix = next_suffix
 
 
 def wrap_text(text, column_width):
@@ -77,30 +82,28 @@ def wrap_text(text, column_width):
 def wrap_words(words, column_width):
     n_words = len(words)
     suffixes = [None] * (n_words + 1)
-    suffixes[n_words] = SuffixSolution(words, range(n_words, n_words), 0, 0, None)
+    suffixes[n_words] = SuffixSolution(Line(words, n_words, n_words, 0), 0, None)
     for i in range(n_words - 1, -1, -1):
-        best = i + 1
-        line_width = calc_line_width(words, range(i, best))
-        weight = calc_line_weight(line_width, column_width, best == n_words)
-        best_weight = weight + suffixes[best].weight()
-        for j in range(i + 2, n_words + 1):
-            width = calc_line_width(words, range(i, j))
-            weight = calc_line_weight(width, column_width, j == n_words)
-            if width > column_width:
-                break
-            new_weight = weight + suffixes[j].weight()
-            if new_weight < best_weight:
-                best = j
-                best_weight = new_weight
-                line_width = width
+        line = Line(words, i, i + 1, len(words[i]))
+        best_weight = line.calc_weight(column_width) + suffixes[line.end].weight
+        best_line = line.copy()
 
-        suffixes[i] = SuffixSolution(words, range(i, best), line_width, best_weight, suffixes[best])
+        for _ in range(i + 2, n_words + 1):
+            line.expand_right()
+            if line.width > column_width:
+                break
+            weight = line.calc_weight(column_width) + suffixes[line.end].weight
+            if weight < best_weight:
+                best_line = line.copy()
+                best_weight = weight
+
+        suffixes[i] = SuffixSolution(best_line, best_weight, suffixes[best_line.end])
 
     lines = []
     suffix = suffixes[0]
-    while not suffix.empty():
-        lines.append(suffix.first_line(column_width))
-        suffix = suffix.next()
+    while not suffix.first_line.empty():
+        lines.append(suffix.first_line.format(column_width))
+        suffix = suffix.next_suffix
 
     return lines
 
